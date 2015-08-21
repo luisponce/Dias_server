9cf6ef5651cc4db006c95a66c4c1280c6edc6058
luis ponce <luisponcekun@gmail.com>
2015-08-21 12:59:10 -0400

    intial server with score controller

---
+++ dias_server/README.md
@@ -0,0 +1,3 @@
+# dias_server
+
+a [Sails](http://sailsjs.org) application
---
+++ dias_server/api/models/Score.js
@@ -0,0 +1,22 @@
+/**
+* Score.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+      playerName:{
+	  type: 'string',
+	  required: true
+      },
+
+      points: {
+	  type: 'int',
+	  required: true
+      }
+  }
+};
+
